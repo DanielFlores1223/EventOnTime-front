a9759cef9affdb8ac912ac24e3edae8a70e3ace8
Fausto Ontiveros <ontiverosfausto@hotmail.com>
2022-08-10 15:34:31 -0500

    Ajustes SIDENAV proveedor, EventService inciado

--- src/app/proveedor/sidebar-prov/sidebar-prov.component.ts
+++ src/app/proveedor/sidebar-prov/sidebar-prov.component.ts
@@ -1,19 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Output } from '@angular/core';
+import { animate, animation, keyframes, style, transition, trigger } from '@angular/animations';
 import { Router } from '@angular/router';
+import { EventEmitter } from '@angular/core';
+
+interface SidebarToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidebar-prov',
   templateUrl: './sidebar-prov.component.html',
-  styleUrls: ['./sidebar-prov.component.scss']
+  styleUrls: ['./sidebar-prov.component.scss'],
+  animations: [
+    trigger('fadeInOut',[
+      transition(':enter',[
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+        )
+      ]),
+      transition(':leave',[
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+        )
+      ])
+    ]),
+    trigger('rotate',[
+      transition(':enter',[
+        animate('1000ms',
+          keyframes([
+            style({transform:'rotate(0deg)',offset: '0'}),
+            style({transform:'rotate(2turn)',offset: '1'})
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class SidebarProvComponent implements OnInit {
 
-  toggle : boolean = false;
+  @Output() onToggleSideNav: EventEmitter<SidebarToggle> = new EventEmitter();
+  @Output() screenWidth = 0;
+  @Output() collapsed = true;
+
+  @HostListener('window:resize',['$event'])onResize(event:any){
+    this.screenWidth= window.innerWidth;
+    if(this.screenWidth <= 768){
+      this.collapsed=false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed,screenWidth: this.screenWidth});
+    }
+  }
+
   navClass = "content";
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    console.log(this.collapsed);
+    console.log(this.screenWidth);
   }
 
   cerrarSesion(){
@@ -22,19 +69,17 @@ export class SidebarProvComponent implements OnInit {
     localStorage.removeItem('role');
     localStorage.removeItem('name');
     localStorage.removeItem('token');
-    //this.ngOnInit();
     this.router.navigate(['/hogar']);
     
   }
+  toggleCollapse(){
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed,screenWidth: this.screenWidth});
+  }
 
-  toggleSideBar(){
-    if(this.toggle==false){
-      this.toggle=true;
-      this.navClass="content-toggle";
-    }else{
-      this.toggle=false;
-      this.navClass="content";
-    }
+  closeSidenav(){
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed,screenWidth: this.screenWidth});
   }
 
 }
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Event } from 'src/app/models/event';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
+  user_token = "";
+
   API_URI = 'https://eventontime.herokuapp.com/api';
 
-  private _registerURL = `${this.API_URI}/event`;
-  private _loginURL = `${this.API_URI}/auth`;
+  private _create_event = `${this.API_URI}/event`;
+  private _get_my_events = `${this.API_URI}/event/my/events?limit=5&from=0&pagination=true`;
+  
+  headers= new HttpHeaders()
+    .set('Authorization', this.getToken());
 
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getToken();
+  }
+
+  getToken(): string{
+    if(localStorage.getItem('token')){
+      return localStorage.getItem('token') || "";
+    }else{
+      return '';
+    }
+  }
+
+  get_user_events(){
+    return this.http.get<any>(this._get_my_events,{ 'headers': this.headers })
+  }
 }
