6473322794a54cc983b0994b6f519e71fd5070e6
DanielFlores1223 <dany-232000@hotmail.com>
2022-08-15 12:24:46 -0500

    dashboard proveedor

--- src/app/proveedor/dashboard-prov/dashboard-prov.component.ts
+++ src/app/proveedor/dashboard-prov/dashboard-prov.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StadisticService } from 'src/app/services/stadistic.service';
 
 @Component({
   selector: 'app-dashboard-prov',
@@ -7,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardProvComponent implements OnInit {
 
+  token = localStorage.getItem('token') ?? '';
 
-  constructor() { }
+  saleData = [
+    { name: "Mobiles", value: 105000 },
+    { name: "Laptop", value: 55000 },
+    { name: "AC", value: 15000 },
+    { name: "Headset", value: 150000 },
+    { name: "Fridge", value: 20000 }
+  ];
+
+  data: any;
+
+  constructor( private stadisticService: StadisticService ) { }
 
   ngOnInit(): void {
+    this.getDash();
+  }
+
+  getDash() {
+    this.stadisticService.getDashboarProviderInfo( this.token ).subscribe(
+      {
+        next: res => {
+          this.data = res.result;
+        },
+        error: err => {
+
+        }
+      }
+    )
   }
 
   
